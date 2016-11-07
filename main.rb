bd4c84baf37817eb3bf93a8640a9cfc1a35fa8ea
Dawei Dai <shachopin@gmail.com>
2016-11-07 15:16:31 -0800

    first

---
+++ main.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+
+before do 
+   headers 'Access-Control-Allow-Origin' => '*', 
+            'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST']  
+end
+
+get "/" do  
+  output = ""
+  params["rows"].to_i.times do
+    params["cols"].to_i.times do
+      output += '*  '
+    end
+    output += '<br/>'
+  end
+  output
+end
+
+# exactly the same as above, only change is from get to post
+
+post "/" do  
+  output = ""
+  params["rows"].to_i.times do
+    params["cols"].to_i.times do
+      output += '*  '
+    end
+    output += '<br/>'
+  end
+  output
+end
